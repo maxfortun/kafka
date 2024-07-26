71b8a70a1501ee166c77bc4f5488859fb6275d7e
Max <max@maxf.net>
2024-07-25 21:54:13 -0400

    updated

--- clients/src/main/java/org/apache/kafka/common/requests/ProduceRequest.java
+++ clients/src/main/java/org/apache/kafka/common/requests/ProduceRequest.java
@@ -42,37 +42,37 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ProduceRequest extends AbstractRequest {
-	public static final Logger log = LoggerFactory.getLogger(ProduceRequest.class);
+    public static final Logger log = LoggerFactory.getLogger(ProduceRequest.class);
 
-	public static final String PRODUCE_REQUEST_PARSER_PROPERTY = "org.apache.kafka.common.requests.ProduceRequestParser";
-	public static final String PRODUCE_REQUEST_PARSER_ENV = "KAFKA_PRODUCE_REQUEST_PARSER";
-	public static final String PRODUCE_REQUEST_PARSER_DEFAULT = "org.apache.kafka.common.requests.ProduceRequestParser";
+    public static final String PRODUCE_REQUEST_PARSER_PROPERTY = "org.apache.kafka.common.requests.ProduceRequestParser";
+    public static final String PRODUCE_REQUEST_PARSER_ENV = "KAFKA_PRODUCE_REQUEST_PARSER";
+    public static final String PRODUCE_REQUEST_PARSER_DEFAULT = "org.apache.kafka.common.requests.ProduceRequestParser";
 
-	private static ProduceRequestParser produceRequestParser = null;
-	static {
-		String produceRequestParserClassName = null;
-		try {
-			produceRequestParserClassName = System.getProperty(PRODUCE_REQUEST_PARSER_PROPERTY);
+    private static ProduceRequestParser produceRequestParser = null;
+    static {
+        String produceRequestParserClassName = null;
+        try {
+            produceRequestParserClassName = System.getProperty(PRODUCE_REQUEST_PARSER_PROPERTY);
 
-			if(null != produceRequestParserClassName) {
-				log.debug("ProduceRequestParser class {} from system property {}", produceRequestParserClassName, PRODUCE_REQUEST_PARSER_PROPERTY);
-			} else {
-				produceRequestParserClassName = System.getenv(PRODUCE_REQUEST_PARSER_ENV);
-			}
+            if (null != produceRequestParserClassName) {
+                log.debug("ProduceRequestParser class {} from system property {}", produceRequestParserClassName, PRODUCE_REQUEST_PARSER_PROPERTY);
+            } else {
+                produceRequestParserClassName = System.getenv(PRODUCE_REQUEST_PARSER_ENV);
+            }
 
-			if(null != produceRequestParserClassName) {
-				log.debug("ProduceRequestParser class {} from env {}", produceRequestParserClassName, PRODUCE_REQUEST_PARSER_ENV);
-			} else {
-				produceRequestParserClassName = PRODUCE_REQUEST_PARSER_DEFAULT;
-				log.debug("ProduceRequestParser class {} default {}", produceRequestParserClassName, PRODUCE_REQUEST_PARSER_DEFAULT);
-			}
+            if (null != produceRequestParserClassName) {
+                log.debug("ProduceRequestParser class {} from env {}", produceRequestParserClassName, PRODUCE_REQUEST_PARSER_ENV);
+            } else {
+                produceRequestParserClassName = PRODUCE_REQUEST_PARSER_DEFAULT;
+                log.debug("ProduceRequestParser class {} default {}", produceRequestParserClassName, PRODUCE_REQUEST_PARSER_DEFAULT);
+            }
 
-			produceRequestParser = (ProduceRequestParser) Class.forName(produceRequestParserClassName).getConstructor().newInstance();
-		} catch(Exception e) {
-			log.error("Failed to initialize {}", produceRequestParserClassName, e);
-			throw new ExceptionInInitializerError(e);
-		}
-	};
+            produceRequestParser = (ProduceRequestParser) Class.forName(produceRequestParserClassName).getConstructor().newInstance();
+        } catch (Exception e) {
+            log.error("Failed to initialize {}", produceRequestParserClassName, e);
+            throw new ExceptionInInitializerError(e);
+        }
+    };
 
     public static Builder forMagic(byte magic, ProduceRequestData data) {
         // Message format upgrades correspond with a bump in the produce request version. Older
