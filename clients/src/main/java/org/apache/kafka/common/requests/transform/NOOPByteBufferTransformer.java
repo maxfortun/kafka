034b6cea7e925b3b8ff0b996e561bc17a49fc41f
Max <max@maxf.net>
2024-08-08 16:36:07 -0400

    updated

--- clients/src/main/java/org/apache/kafka/common/requests/transform/NOOPByteBufferTransformer.java
+++ clients/src/main/java/org/apache/kafka/common/requests/transform/NOOPByteBufferTransformer.java
@@ -37,7 +37,7 @@ public class NOOPByteBufferTransformer implements ByteBufferTransformer {
         int prevPosition = byteBuffer.position();
 
         if (log.isTraceEnabled()) {
-            log.trace("{}: Returning buffer as-is {} {}", transformerName, StandardCharsets.UTF_8.decode(byteBuffer).toString(), e);
+            log.trace("{}: Returning buffer as-is {} {}", transformerName, StandardCharsets.UTF_8.decode(byteBuffer).toString());
         } else {
             log.debug("{}: Returning buffer as-is {}", transformerName, byteBuffer);
         }
